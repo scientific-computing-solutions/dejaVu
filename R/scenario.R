6c7f2dbe7bbdd6333053dd496487c44240457aae
Nik_Burkoff_Tessella <Nik.Burkoff@astrazeneca.com>
2015-12-04 15:22:37 +0100

    simulate: add scenario summary

--- R/scenario.R
+++ R/scenario.R
@@ -8,22 +8,16 @@ NULL
 ##' @export
 as.data.frame.Scenario <- function(x,use.adjusted.pval=FALSE,...){
   
-  if(!is.logical(use.adjusted.pval) || length(use.adjusted.pval)>1){
-    stop("Invalid argument use.adjusted.pval")
-  }
-  
-  if(use.adjusted.pval && class(x[[1]])=="summary.SingleSimFit") {
-    stop("Invalid arguments: cannot use adjusted.pval if multiple imputed test statistic is not used")
-  }
+  Validate.adjusted.pval(x$summaries,use.adjusted.pval)
   
   .extract <- function(name,fun.val=numeric(1)){
-    vapply(x,function(x){x[[name]]},FUN.VALUE = fun.val)
+    vapply(x$summaries,function(y){y[[name]]},FUN.VALUE = fun.val)
   }
   
   pval.string <-if(use.adjusted.pval)  "adjusted.pval" else "pval"
   
   
-  data.frame(replica=1:length(x),
+  data.frame(replica=1:length(x$summaries),
              treatment.effect=.extract("treatment.effect"),
              se=.extract("se"),
              pval=.extract(pval.string),
@@ -33,11 +27,23 @@ as.data.frame.Scenario <- function(x,use.adjusted.pval=FALSE,...){
 }
 
 
+Validate.adjusted.pval <- function(x,use.adjusted.pval){
+  if(!is.logical(use.adjusted.pval) || length(use.adjusted.pval)>1){
+   stop("Invalid argument use.adjusted.pval")
+  }
+
+  if(use.adjusted.pval && class(x[[1]])=="summary.SingleSimFit") {
+    stop("Invalid arguments: cannot use adjusted.pval if multiple imputed test statistic is not used")
+  }
+}
+
 
 ##' Boo
 ##' @export
-CreateScenario <- function(object){
+CreateScenario <- function(object,description=""){
 
+  #validate description
+  
   if(!is.list(object)){
     stop("Invalid argument: object must be a list of either summary.ImputeSimFit or summary.SingleSimFit")
   }    
@@ -47,6 +53,49 @@ CreateScenario <- function(object){
     stop("Invalid argument: object must be a list of either summary.ImputeSimFit or summary.SingleSimFit")
   }
   
-  class(object) <- "Scenario"                             
-  object                             
+  retVal <- list(description=description,
+                 summaries=object)
+  class(retVal) <- "Scenario"                             
+  retVal                             
+}
+
+
+##' summary.Scenario object
+##' 
+##' TODO
+##' @name summary.Scenario.object
+NULL 
+
+##' @export
+summary.Scenario <- function(object,alpha=0.05,use.adjusted.pval=FALSE,...){
+  Validate.adjusted.pval(object$summaries,use.adjusted.pval)
+  
+  data <- as.data.frame.Scenario(object,use.adjusted.pval)
+  
+  retVal <- list(treatment.effect=exp(mean(log(data$treatment.effect))),
+                 se=mean(data$se),
+                 power=sum(data$pval<alpha)/nrow(data),
+                 alpha=alpha,
+                 use.adjusted.pval=use.adjusted.pval,
+                 description=object$description)
+  
+  class(retVal) <- "summary.Scenario"
+  retVal 
+}
+
+
+##' @export
+print.summary.Scenario <- function(x,...){
+  cat("Scenario summary\n")
+  if(!x$description==""){
+    cat("Description:",x$description,fill=TRUE)
+  }
+  cat("Estimated Treatment Effect:",x$treatment.effect,fill=TRUE)
+  cat("Event Rate Reduction: ",100*(1-x$treatment.effect) ,"%\n",sep="")
+  cat("Estimated SE (log) treatment effect:",x$se,fill=TRUE)
+  cat("Using alpha=",x$alpha,sep="")
+  if(x$use.adjusted.pval){
+    cat("(and an adjusted number of degrees of freedom)")
+  }
+  cat("\n  power:",x$power,fill=TRUE)
 }
