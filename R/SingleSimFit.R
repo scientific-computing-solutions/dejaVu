211d9e04d46059a6e2fb3faa5fe1a6ead26c3108
Nik_Burkoff_Tessella <Nik.Burkoff@astrazeneca.com>
2015-12-04 09:53:47 +0100

    simulate: tweak summary of SingleSimFit

--- R/SingleSimFit.R
+++ R/SingleSimFit.R
@@ -13,22 +13,27 @@ NULL
 NULL
 
 ##' @export
-summary.SingleSimFit <- function(object,...){
+summary.SingleSimFit <- function(object,CI.limit=0.95,...){
   if(!object$equal.dispersion){
     stop("Cannot generate a summary if equal.dispersion is FALSE")
   }  
- 
+
+  if(!is.numeric(CI.limit) || is.na(CI.limit) || length(CI.limit)>1 || CI.limit < 0 || CI.limit>1){
+    stop("Invalid argument CI.limit")
+  } 
+   
   model.summary <- summary(object$model)
   
   retVal <- list(model.summary=model.summary,
-                 rate.ratio=exp(model.summary$coefficient[2,1]),
-                 se.corrected=model.summary$coefficient[2,2]*exp(model.summary$coefficient[2,1]),
-                 se.logrates=model.summary$coefficient[2,2], #se2
+                 treatment.effect=exp(model.summary$coefficient[2,1]),
+                 CI.limit=CI.limit,
+                 CI=exp(model.summary$coefficient[2,1]+ c(-1,1)*qnorm(1-CI.limit/2)*model.summary$coefficient[2,2]),
+                 se=model.summary$coefficient[2,2], #se 2 from old code
                  theta=model.summary$theta, #only if negative binomial
-                 exp.intercept=exp(model.summary$coefficient[1,1])*c(1,exp(model.summary$coefficient[2,1])),         
+                 rate.estimate=exp(model.summary$coefficient[1,1])*c(1,exp(model.summary$coefficient[2,1])),         
                  pval=model.summary$coefficient[2,4],
                  datastatus=object$singleSim$status
-                 )# exp.intercept this was nbd.mp and nbd.mt in old code 
+                 ) 
   
   class(retVal) <- "summary.SingleSimFit"
   retVal
@@ -37,10 +42,10 @@ summary.SingleSimFit <- function(object,...){
 ##' @export
 print.summary.SingleSimFit <- function(x,...){
   cat("Summary for model fit for",x$datastatus,"data set",fill = TRUE)
-  cat("Ratio Rate:",x$rate.ratio,fill=TRUE)
-  cat("SE corrected:",x$se.corrected,fill=TRUE)
-  cat("SE logrates:",x$se.logrates,fill=TRUE)
-  cat("exp intercept:",x$exp.intercept,fill=TRUE)
+  cat("Treatment Effect:",x$treatment.effect,fill=TRUE)
+  cat("SE (log) treatment effect:",x$se,fill=TRUE)
+  cat(x$CI.limit*100,"% CI: [",x$CI[1],", ",x$CI[2],"]", sep="",fill=TRUE)
+  cat("Rate Estimates (per arm):",x$rate.estimate,fill=TRUE)
   if(!is.null(x$theta)){
     cat("Negative binomial model parameter theta:",x$theta,fill=TRUE)
   }
